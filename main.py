b2ceb4ea11a4b0f63f4dae474700eacc63cbb90d
yuinakorn@hotmail.com <yuinakorn@hotmail.com>
2022-09-28 17:35:27 +0700

    update key

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form, HTTPException
 import requests
 import os
 from dotenv import dotenv_values
 from datetime import datetime, timezone, timedelta
-
 
 config_env = {
     **dotenv_values(".env"),
@@ -13,119 +12,127 @@ config_env = {
 app = FastAPI()
 
 
-@app.get("/noline")
-async def noline():
-    global error_hos, jdata
-    url = config_env['URL1']
+@app.get("/noline/t/{token}")
+async def noline(token: str = None):
+    if token == config_env['TOKEN']:
+        global error_hos, jdata
+        url = config_env['URL1']
 
-    headers = {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
-    payload = {}
-    response = requests.request("GET", url, headers=headers, data=payload)
-    json_data = response.json()
-    error_hos = []
-    i = 0
-    e = 0
-    for data in json_data:
-        urls = f"{config_env['URL2']}{data['hcode']}"
         headers = {
             'Content-Type': 'application/x-www-form-urlencoded'
         }
         payload = {}
-        try:
-            response = requests.request("GET", urls, headers=headers, data=payload)
-            json_data = response.json()
+        response = requests.request("GET", url, headers=headers, data=payload)
+        json_data = response.json()
+        error_hos = []
+        i = 0
+        e = 0
+        for data in json_data:
+            urls = f"{config_env['URL2']}{data['hcode']}"
+            headers = {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+            payload = {}
+            try:
+                response = requests.request("GET", urls, headers=headers, data=payload)
+                json_data = response.json()
 
-            # because of the source is different type of data
-            if type(json_data) == dict:
-                jdata = json_data['status']
-            elif type(json_data) == list:
-                jdata = json_data[0]['status']
+                # because of the source is different type of data
+                if type(json_data) == dict:
+                    jdata = json_data['status']
+                elif type(json_data) == list:
+                    jdata = json_data[0]['status']
 
-            print(data['hcode'], " ", data['hname'], jdata)
+                print(data['hcode'], " ", data['hname'], jdata)
 
-            if jdata != 'OK':
-                error_hos.append(f"{data['hcode']} {data['hname']} {jdata}")
-                e += 1
+                if jdata != 'OK':
+                    error_hos.append(f"{data['hcode']} {data['hname']} {jdata}")
+                    e += 1
 
-        except Exception as err:
-            print(err)
-            pass
+            except Exception as err:
+                print(err)
+                pass
 
-        i += 1
+            i += 1
 
-    error_hos.append(f"Total {i} hospital {e} error")
+        error_hos.append(f"Total {i} hospital {e} error")
 
-    return str(error_hos).encode('utf-8')
+        return str(error_hos).encode('utf-8')
+
+    else:
+        raise HTTPException(status_code=404, detail="Token Invalid")
 
 
 @app.post("/")
-async def root():
-    global error_hos, jdata
-    url = config_env['URL1']
+async def root(keys: str = Form()):
+    if keys == config_env['API_KEY']:
+        global error_hos, jdata
+        url = config_env['URL1']
 
-    headers = {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
-    payload = {}
-    response = requests.request("GET", url, headers=headers, data=payload)
-    json_data = response.json()
-    error_hos = []
-    i = 0
-    e = 0
-    for data in json_data:
-        if str(data['hcode'])[0] == '0':
-            continue
-        urls = f"{config_env['URL2']}{data['hcode']}"
         headers = {
             'Content-Type': 'application/x-www-form-urlencoded'
         }
         payload = {}
-        try:
-            response = requests.request("GET", urls, headers=headers, data=payload)
-            json_data = response.json()
+        response = requests.request("GET", url, headers=headers, data=payload)
+        json_data = response.json()
+        error_hos = []
+        i = 0
+        e = 0
+        for data in json_data:
+            if str(data['hcode'])[0] == '0':
+                continue
+            urls = f"{config_env['URL2']}{data['hcode']}"
+            headers = {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+            payload = {}
+            try:
+                response = requests.request("GET", urls, headers=headers, data=payload)
+                json_data = response.json()
 
-            # because of the source is different type of data
-            if type(json_data) == dict:
-                jdata = json_data['status']
-            elif type(json_data) == list:
-                jdata = json_data[0]['status']
+                # because of the source is different type of data
+                if type(json_data) == dict:
+                    jdata = json_data['status']
+                elif type(json_data) == list:
+                    jdata = json_data[0]['status']
 
-            print(data['hcode'], " ", data['hname'], jdata)
+                print(data['hcode'], " ", data['hname'], jdata)
 
-            if jdata != 'OK':
-                error_hos.append(f"{data['hcode']} {data['hname']} {jdata}")
-                e += 1
+                if jdata != 'OK':
+                    error_hos.append(f"{data['hcode']} {data['hname']} {jdata}")
+                    e += 1
 
-        except Exception as err:
-            print(err)
-            pass
+            except Exception as err:
+                print(err)
+                pass
 
-        i += 1
+            i += 1
 
-    error_hos.append(f"Total {i} hospital {e} error")
+        error_hos.append(f"Total {i} hospital {e} error")
 
-    str_trim = str(error_hos).replace('[', '').replace(']', '').replace("'", '').replace(",", '\n')
-    str_trim = str("\n" + " " + str_trim)
+        str_trim = str(error_hos).replace('[', '').replace(']', '').replace("'", '').replace(",", '\n')
+        str_trim = str("\n" + " " + str_trim)
 
-    # now = datetime.now().strftime("%d/%m/%Y %H:%M")
-    # now = datetime.datetime.now(LOCAL_TIMEZONE).strftime("%d/%m/%Y %H:%M")
-    tz = timezone(timedelta(hours=7))
-    now = datetime.now(tz=tz).strftime("%d/%m/%Y %H:%M")
-    str_trim = str("\n" + " Report at " + now + str_trim)
+        # now = datetime.now().strftime("%d/%m/%Y %H:%M")
+        # now = datetime.datetime.now(LOCAL_TIMEZONE).strftime("%d/%m/%Y %H:%M")
+        tz = timezone(timedelta(hours=7))
+        now = datetime.now(tz=tz).strftime("%d/%m/%Y %H:%M")
+        str_trim = str("\n" + " Report at " + now + str_trim)
 
-    url = config_env['LINE_NOTIFY']
+        url = config_env['LINE_NOTIFY']
 
-    payload = {"message": str(str_trim)}
-    headers = {
-        'Authorization': 'Bearer ' + config_env['LINE_TOKEN'],
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
+        payload = {"message": str(str_trim)}
+        headers = {
+            'Authorization': 'Bearer ' + config_env['LINE_TOKEN'],
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
 
-    # call LINE Notify API
-    requests.request("POST", url, headers=headers, data=payload)
+        # call LINE Notify API
+        requests.request("POST", url, headers=headers, data=payload)
 
-    # print(response.text)
+        # print(response.text)
 
-    return str(error_hos).encode('utf-8')
+        return str(error_hos).encode('utf-8')
+
+    else:
+        return "Wrong API key"
