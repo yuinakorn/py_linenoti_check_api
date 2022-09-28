da69077157744f02548c29ea22e49d63662b4ffc
yuinakorn@hotmail.com <yuinakorn@hotmail.com>
2022-09-28 12:04:02 +0700

    update quiet api

--- caller.py
+++ caller.py
@@ -1,13 +1,18 @@
-#!/usr/bin/env python3.10
+#!/Docker/py_linenoti_check_api/env/lib/ python3.10
 # In the first line check your version python such as python3.10
 import requests
+import os
+from dotenv import dotenv_values
 
-url = ""
+config_env = {
+    **dotenv_values(".env"),
+    **os.environ,
+}
+
+url = config_env['API_URL']
 
 # call the api but not wait for the response
 try:
     requests.get(url, timeout=0.1)
 except requests.exceptions.ReadTimeout:
     pass
-
-
--- main.py
+++ main.py
@@ -14,6 +14,53 @@ config_env = {
 app = FastAPI()
 
 
+@app.get("/noline")
+async def noline():
+    global error_hos, jdata
+    url = config_env['URL1']
+
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    payload = {}
+    response = requests.request("GET", url, headers=headers, data=payload)
+    json_data = response.json()
+    error_hos = []
+    i = 0
+    e = 0
+    for data in json_data:
+        urls = f"{config_env['URL2']}{data['hcode']}"
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        payload = {}
+        try:
+            response = requests.request("GET", urls, headers=headers, data=payload)
+            json_data = response.json()
+
+            # because of the source is different type of data
+            if type(json_data) == dict:
+                jdata = json_data['status']
+            elif type(json_data) == list:
+                jdata = json_data[0]['status']
+
+            print(data['hcode'], " ", data['hname'], jdata)
+
+            if jdata != 'OK':
+                error_hos.append(f"{data['hcode']} {data['hname']} {jdata}")
+                e += 1
+
+        except Exception as err:
+            print(err)
+            pass
+
+        i += 1
+
+    error_hos.append(f"Total {i} hospital {e} error")
+
+    return str(error_hos).encode('utf-8')
+
+
 @app.get("/")
 async def root():
     global error_hos, jdata
@@ -75,8 +122,9 @@ async def root():
         'Content-Type': 'application/x-www-form-urlencoded'
     }
 
-    response = requests.request("POST", url, headers=headers, data=payload)
+    # call LINE Notify API
+    requests.request("POST", url, headers=headers, data=payload)
 
-    print(response.text)
+    # print(response.text)
 
     return str(error_hos).encode('utf-8')
